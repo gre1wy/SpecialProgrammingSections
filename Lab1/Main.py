3ce323948e0c5f107fc4bce63da0652a397bdfa4
Yaroslav Grunda <119404378+gre1wy@users.noreply.github.com>
2023-10-04 01:46:17 +0300

    Update Main.py

--- Lab1/Main.py
+++ Lab1/Main.py
@@ -101,37 +101,6 @@ def download():
     for i in range(1, 28):
         download_vhi_file(i)
 
-
-def transform_regions_NOAA_to_ua(index):
-    if index == 1: return 22
-    elif index == 2: return 24
-    elif index == 3: return 23
-    elif index == 4: return 25
-    elif index == 5: return 3
-    elif index == 6: return 4
-    elif index == 7: return 8
-    elif index == 8: return 19
-    elif index == 9: return 20
-    elif index == 10: return 21
-    elif index == 11: return 9
-    elif index == 12: return 90
-    elif index == 13: return 10
-    elif index == 14: return 11
-    elif index == 15: return 12
-    elif index == 16: return 13
-    elif index == 17: return 14
-    elif index == 18: return 15
-    elif index == 19: return 16
-    elif index == 20: return 250
-    elif index == 21: return 17
-    elif index == 22: return 18
-    elif index == 23: return 6
-    elif index == 24: return 1
-    elif index == 25: return 2
-    elif index == 26: return 7
-    elif index == 27: return 5
-
-
 # Функція для зчитування файлів VHI-індексу у фрейм pandas
 def read_vhi_files(directory):
     vhi_data = pd.DataFrame()
