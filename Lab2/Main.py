b7512f09731d956da95db1bbe5b7f5be40d5e33e
gre1wy <grundayaroslav28@gmail.com>
2023-10-26 00:02:30 +0300

    add multiple ghraphic

--- Lab2/Main.py
+++ Lab2/Main.py
@@ -33,7 +33,9 @@ dict_of_areas= {1:"Вінницька",
     25:"Крим",
     26:"Київ",
     27:"Севастополь"}
-
+dict_of_areas_with_0 = dict_of_areas.copy()
+dict_of_areas_with_0[0] = 'Нічого'
+dict_of_areas_with_0 = dict(sorted(dict_of_areas_with_0.items()))
 def download_data():
     time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     data_dir = 'data'
@@ -58,111 +60,64 @@ def download_data():
         with open(filename, 'wb') as file:
             file.write(clean_text)
 
-
-def read_vhi_files(directory):
-    vhi_data = pd.DataFrame()
-
-    # Отримуємо список файлів у заданій директорії
-    files = os.listdir(directory)
-
-    for file in files:
-        if file.startswith('vhi_data_province'):
-            # Зчитуємо файл у фрейм pandas
-            file_path = os.path.join(directory, file)
-            df = pd.read_csv(file_path, index_col=None, header=1)
-            df = df.drop(df.loc[df['VHI'] == -1].index)
-            province_id = int(file.split('_')[3])
-            df.insert(0, 'area', province_id)
-            vhi_data = pd.concat([vhi_data, df], ignore_index=True)
-    dict_for_transfer = {
-        1: 22,
-        2: 24,
-        3: 23,
-        4: 25,
-        5: 3,
-        6: 4,
-        7: 8,
-        8: 19,
-        9: 20,
-        10: 21,
-        11: 9,
-        12: 26,
-        13: 10,
-        14: 11,
-        15: 12,
-        16: 13,
-        17: 14,
-        18: 15,
-        19: 16,
-        20: 27,
-        21: 17,
-        22: 18,
-        23: 6,
-        24: 1,
-        25: 2,
-        26: 7,
-        27: 5
-
-    }
-    vhi_data["area"].replace(dict_for_transfer, inplace=True)
-    vhi_data.sort_values(by=['area', 'year', 'week'], ascending=True, inplace=True)
-    vhi_data = vhi_data.reset_index(drop=True)
-    return vhi_data
-
-#download_data()
-#vhi_data = read_vhi_files(r'data')
 class StockExample(server.App):
     title = 'NOAA data vizualization'
 
     inputs = [
         {
             "type": 'dropdown',
-            "label": 'Вибрати дані',
+            "label": 'Вибрати дані(1)',
             "options": [{'label': "VCI", "value": 'VCI'},
                         {'label': "TCI", "value": 'TCI'},
                         {'label': "VHI", "value": 'VHI'}],
-            "key": 'ticker',
+            "key": 'ticker1',
             "action_id": "update_data"
         },
         {
             "type": 'dropdown',
-            "label": 'Адміністративна одиниця',
-            "options": [
-                {'label': "Вінницька", "value": "1"},
-                {'label': "Волинська", "value":  "2"},
-                {'label': "Дніпропетровська", "value": "3"},
-                {'label': "Донецька", "value": "4"},
-                {'label': "Житомирська", "value": "5"},
-                {'label': "Закарпатська", "value": "6"},
-                {'label': "Запорізька", "value": "7"},
-                {'label': "Івано-Франківська", "value": "8"},
-                {'label': "Київська", "value": "9"},
-                {'label': "Кіровоградська", "value": "10"},
-                {'label': "Луганська", "value": "11"},
-                {'label': "Львівська", "value": "12"},
-                {'label': "Миколаївська", "value": "13"},
-                {'label': "Одеська", "value": "14"},
-                {'label': "Полтавська", "value": "15"},
-                {'label': "Рівненська", "value": "16"},
-                {'label': "Сумська", "value": "17"},
-                {'label': "Тернопольска", "value": "18"},
-                {'label': "Харківська", "value": "19"},
-                {'label': "Херсонська", "value": "20"},
-                {'label': "Хмельницька", "value": "21"},
-                {'label': "Черкаська", "value": "22"},
-                {'label': "Чернігівська", "value": "23"},
-                {'label': "Чернівецька", "value": "24"},
-                {'label': "Крим", "value": "25"},
-                {'label': "Київ", "value": "26"},
-                {'label': "Севастополь", "value": "27"},
-            ],
+            "label": 'Вибрати дані(2)',
+            "options": [{'label': "Нічого", "value": '0'},
+                        {'label': "VCI", "value": 'VCI'},
+                        {'label': "TCI", "value": 'TCI'},
+                        {'label': "VHI", "value": 'VHI'}],
+            "key": 'ticker2',
+            "action_id": "update_data"
+        },
+        {
+            "type": 'dropdown',
+            "label": 'Вибрати дані(3)',
+            "options": [{'label': "Нічого", "value": '0'},
+                        {'label': "VCI", "value": 'VCI'},
+                        {'label': "TCI", "value": 'TCI'},
+                        {'label': "VHI", "value": 'VHI'}],
+            "key": 'ticker3',
+            "action_id": "update_data"
+        },
+        {
+            "type": 'dropdown',
+            "label": 'Адміністративна одиниця(1)',
+            "options": [{'label': dict_of_areas[i], 'value': i} for i in dict_of_areas],
             "key": 'selected_region',
+            "action_id": "update_data"
+        },
+        {
+            "type": 'dropdown',
+            "label": 'Адміністративна одиниця(2) для порівняння',
+            "options": [{'label': dict_of_areas_with_0[i], 'value': i} for i in dict_of_areas_with_0],
+            "key": 'selected_region_2',
+            "action_id": "update_data"
+        },
+        {
+            "type": 'dropdown',
+            "label": 'Адміністративна одиниця(3) для порівняння',
+            "options": [{'label': dict_of_areas_with_0[i], 'value': i} for i in dict_of_areas_with_0],
+            "key": 'selected_region_3',
             "action_id": "update_data"
         },
         {
             "type": 'text',
             "label": "Week range",
-            "value": "1-48",
+            "value": "1-52",
             "key": "wrange",
             "action_id": "update_data"
 
@@ -171,14 +126,14 @@ class StockExample(server.App):
             "type": 'text',
             "label": "Year range",
             "value": "1983-2023",
-            "key": "rrange",
+            "key": "yrange",
             "action_id": "update_data"
 
         },
         {
             "type": 'text',
             "label": "xticks step",
-            "value": "1",
+            "value": "27",
             "key": "step",
             "action_id": "update_data"
         },
@@ -186,55 +141,210 @@ class StockExample(server.App):
     ]
     controls = [{"type": "hidden",
                  "id": "update_data"}]
-    tabs = ["Table", "Plot"]
+    tabs = ["Table1", "Table2", "Table3", "Plot"]
     outputs = [{"type": "table",
-                "id": "table_id",
+                "id": "data1",
                 "control_id": "update_data",
-                "tab": "Table",
+                "tab": "Table1",
                 "on_page_load": True},
+               {"type": "table",
+                "id": "data2",
+                "control_id": "update_data",
+                "tab": "Table2"},
+               {"type": "table",
+                "id": "data3",
+                "control_id": "update_data",
+                "tab": "Table3"},
                {"type": "plot",
                 "id": "plot",
                 "control_id": "update_data",
                 "tab": "Plot"}
                ]
 
-    def getData(self, params):
-        ticker = params['ticker']
+    def data1(self, params):
+        ticker1 = params['ticker1']
+        ticker2 = params['ticker2']
+        ticker3 = params['ticker3']
         selected_region = params['selected_region']
         wrange = params['wrange']
-        rrange = params['rrange']
-        start_year = rrange.split('-')[0]
-        end_year = rrange.split('-')[1]
+        yrange = params['yrange']
+
+        start_year = yrange.split('-')[0]
+        end_year = yrange.split('-')[1]
         start_week = wrange.split('-')[0]
         end_week = wrange.split('-')[1]
-        df = pd.read_csv(f'data/vhi_data_province_{selected_region}_2023-10-20_00-53-14.csv', index_col=False, header=1, skiprows=0)
-        df = df.drop(df.loc[df['VHI'] == -1].index)
-        df = df[(df['year'] >= int(start_year)) & (df['year'] <= int(end_year)) & (df['week'] >= int(start_week)) & (df['week'] <= int(end_week))][['year', 'week', str(ticker)]]
-        df['year:week'] = df['year'].astype(str) + ':' + df['week'].astype(str)
-        return df
 
-    def getPlot(self, params):
-        df = self.getData(params)
+        df = pd.read_csv(f'data/vhi_data_province_{selected_region}_2023-10-20_00-53-14.csv',
+                        index_col=False, header=1, skiprows=0)
+        df = df.drop(df.loc[df['VHI'] == -1].index)
+        df = df[(df['year'] >= int(start_year)) & (df['year'] <= int(end_year)) & (df['week'] >= int(start_week)) & (
+                    df['week'] <= int(end_week))]
+
+
+        ticker2_checked = None
+        ticker3_checked = None
+        if ticker2 != '0':
+            ticker2_checked = str(ticker2)
+        if ticker3 != '0':
+            ticker3_checked = str(ticker3)
+        list_to_show = ['year', 'week', str(ticker1)]
+        if ticker2_checked and ticker2_checked != ticker1:
+            list_to_show.append(ticker2_checked)
+        if ticker3_checked and ticker3_checked != ticker1:
+            list_to_show.append(ticker3_checked)
+        if list_to_show[-1] == list_to_show[-2]:
+            list_to_show.pop(-1)
+
+        df = df[list_to_show]
+        return df
+    def data2(self,params):
+        ticker1 = params['ticker1']
+        ticker2 = params['ticker2']
+        ticker3 = params['ticker3']
+        selected_region_2 = params['selected_region_2']
+        wrange = params['wrange']
+        yrange = params['yrange']
+
+        start_year = yrange.split('-')[0]
+        end_year = yrange.split('-')[1]
+        start_week = wrange.split('-')[0]
+        end_week = wrange.split('-')[1]
+
+        df_2 = pd.read_csv(f'data/vhi_data_province_{selected_region_2}_2023-10-20_00-53-14.csv',
+                         index_col=False, header=1, skiprows=0)
+        df_2 = df_2.drop(df_2.loc[df_2['VHI'] == -1].index)
+        df_2 = df_2[(df_2['year'] >= int(start_year)) & (df_2['year'] <= int(end_year)) & (df_2['week'] >= int(start_week)) & (
+                df_2['week'] <= int(end_week))]
+
+        ticker2_checked = None
+        ticker3_checked = None
+        if ticker2 != '0':
+            ticker2_checked = str(ticker2)
+        if ticker3 != '0':
+            ticker3_checked = str(ticker3)
+        list_to_show = ['year', 'week', str(ticker1)]
+        if ticker2_checked and ticker2_checked != ticker1:
+            list_to_show.append(ticker2_checked)
+        if ticker3_checked and ticker3_checked != ticker1:
+            list_to_show.append(ticker3_checked)
+        if list_to_show[-1] == list_to_show[-2]:
+            list_to_show.pop(-1)
+
+        df_2 = df_2[list_to_show]
+        return df_2
+    def data3(self, params):
+        ticker1 = params['ticker1']
+        ticker2 = params['ticker2']
+        ticker3 = params['ticker3']
+        selected_region_3 = params['selected_region_3']
+        wrange = params['wrange']
+        yrange = params['yrange']
+
+        start_year = yrange.split('-')[0]
+        end_year = yrange.split('-')[1]
+        start_week = wrange.split('-')[0]
+        end_week = wrange.split('-')[1]
+
+        df_3 = pd.read_csv(f'data/vhi_data_province_{selected_region_3}_2023-10-20_00-53-14.csv',
+                           index_col=False, header=1, skiprows=0)
+        df_3 = df_3.drop(df_3.loc[df_3['VHI'] == -1].index)
+        df_3 = df_3[(df_3['year'] >= int(start_year)) & (df_3['year'] <= int(end_year)) & (df_3['week'] >= int(start_week)) & (
+                    df_3['week'] <= int(end_week))]
+
+        ticker2_checked = None
+        ticker3_checked = None
+        if ticker2 != '0':
+            ticker2_checked = str(ticker2)
+        if ticker3 != '0':
+            ticker3_checked = str(ticker3)
+        list_to_show = ['year', 'week', str(ticker1)]
+        if ticker2_checked and ticker2_checked != ticker1:
+            list_to_show.append(ticker2_checked)
+        if ticker3_checked and ticker3_checked != ticker1:
+            list_to_show.append(ticker3_checked)
+        if list_to_show[-1] == list_to_show[-2]:
+            list_to_show.pop(-1)
+
+        df_3 = df_3[list_to_show]
+        return df_3
+    def plot(self, params):
+        df = self.data1(params)
+        df['year:week'] = df['year'].astype(str) + ':' + df['week'].astype(str)
+        try:
+            df_2 = self.data2(params)
+            df_2['year:week'] = df_2['year'].astype(str) + ':' + df_2['week'].astype(str)
+        except: print("В другій вибрано нічого")
+        try:
+            df_3 = self.data3(params)
+            df_3['year:week'] = df_3['year'].astype(str) + ':' + df_3['week'].astype(str)
+        except: print("В третій вибрано нічого")
+
         plt.figure(figsize=(16, 6))
-        #plt.legend()
-        img = sns.lineplot(data=df, x='year:week', y=f'{params["ticker"]}', marker="o", markersize=5)
+        img = sns.lineplot(data=df, x='year:week', y=f'{params["ticker1"]}',
+                           label=f'{params["ticker1"]+dict_of_areas[int(params["selected_region"])]}',
+                           marker="o", markersize=3)
+
+        if params["selected_region_2"] != '0':
+            sns.lineplot(data=df_2, x='year:week', y=f'{params["ticker1"]}',
+                         label=f'{params["ticker1"]+dict_of_areas[int(params["selected_region_2"])]}', marker="o", markersize=3, ax=img)
+        if params["selected_region_3"] != "0":
+            sns.lineplot(data=df_3, x='year:week', y=f'{params["ticker1"]}',
+                         label=f'{params["ticker1"]+dict_of_areas[int(params["selected_region_3"])]}', marker="o", markersize=3, ax=img)
+
+        if str(params["ticker2"]) != "0":
+            sns.lineplot(data=df, x='year:week', y=f'{params["ticker2"]}',
+                         label=f'{params["ticker2"]+dict_of_areas[int(params["selected_region"])]}', marker="o", markersize=3, ax=img)
+            if str(params["selected_region_2"]) != "0":
+                sns.lineplot(data=df_2, x='year:week', y=f'{params["ticker2"]}',
+                             label=f'{params["ticker2"]+dict_of_areas[int(params["selected_region_2"])]}', marker="o", markersize=3, ax=img)
+            if str(params["selected_region_3"]) != "0":
+                sns.lineplot(data=df_3, x='year:week', y=f'{params["ticker2"]}',
+                             label=f'{params["ticker2"]+dict_of_areas[int(params["selected_region_3"])]}', marker="o", markersize=3, ax=img)
+        if str(params["ticker3"]) != "0":
+            sns.lineplot(data=df, x='year:week', y=f'{params["ticker3"]}',
+                         label=f'{params["ticker3"]+dict_of_areas[int(params["selected_region"])]}', marker="o", markersize=3, ax=img)
+            if str(params["selected_region_3"]) != "0":
+                sns.lineplot(data=df_3, x='year:week', y=f'{params["ticker3"]}',
+                             label=f'{params["ticker3"]+dict_of_areas[int(params["selected_region_2"])]}', marker="o", markersize=3, ax=img)
+            if str(params["selected_region_2"]) != "0":
+                sns.lineplot(data=df_2, x='year:week', y=f'{params["ticker3"]}',
+                             label=f'{params["ticker3"]+dict_of_areas[int(params["selected_region_3"])]}', marker="o", markersize=3, ax=img)
+
+        plt.xlabel('Year:Week')  # Label for the x-axis
+        plt.ylabel('Value')  # Label for the y-axis
         plt.xticks(range(0, len(df), int(params['step'])))
         plt.xticks(rotation=55)
-        img.set_title('Plot1')
+        plt.legend()
         return img
 
 
 app = StockExample()
-app.launch(port=9099)
+app.launch(port=9105)
 """
 df = pd.read_csv(f'data/vhi_data_province_1_2023-10-20_00-53-14.csv', index_col=False, header=1, skiprows=0)
+df_2 = pd.read_csv(f'data/vhi_data_province_2_2023-10-20_00-53-14.csv', index_col=False, header=1, skiprows=0)
 df = df.drop(df.loc[df['VHI'] == -1].index)
+df_2 = df_2.drop(df_2.loc[df_2['VHI'] == -1].index)
+
 start_year = 2020
 end_year = 2022
 start_week = 20
 end_week = 30
-print(df)
-df["year-week"] = df['year'].astype(str) +"-"+ df["week"].astype(str)
-print(df)
-print(df[(df['year'] >= start_year) & (df['year'] <= end_year) & (df['week'] >= start_week) & (df['week'] <= end_week)])
-"""
+
+df["year:week"] = df['year'].astype(str) + ":" + df["week"].astype(str)
+df_2["year:week"] = df_2['year'].astype(str) + ":" + df_2["week"].astype(str)
+
+df = df[(df['year'] >= start_year) & (df['year'] <= end_year) & (df['week'] >= start_week) & (df['week'] <= end_week)]
+df_2 = df_2[(df_2['year'] >= start_year) & (df_2['year'] <= end_year) & (df_2['week'] >= start_week) & (df_2['week'] <= end_week)]
+
+plt.figure(figsize=(16, 6))
+sns.lineplot(data=df, x='year:week', y="VHI", label="Province 1", marker="o", markersize=5)
+sns.lineplot(data=df_2, x='year:week', y="VHI", label="Province 2", marker="o", markersize=5)
+plt.xticks(range(0, len(df), 27))
+plt.xticks(rotation=55)
+plt.title('Plot1')
+plt.legend()
+plt.show()"""
+
+
+
